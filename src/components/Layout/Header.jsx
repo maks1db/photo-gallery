2e9084912738d0144c3a35c7a2b3cec3365f44cd
Maksim Skvortsov <maksskvor@yandex.ru>
2017-09-26 09:12:21 +0300

    add onScroll events

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import styles from './Header.scss';
 import NavLi from './NavLi.jsx';
+import ClassName from 'className.js';
 
-export default (props) => (
-    <div className={styles.header}>
-        <div className={styles.title}>{props.title}</div> 
-        <nav className={styles.navigation}>
-            <ul>
-                {props.showMainLink && <NavLi href='/' title="Главная" />}
-                <NavLi title="О нас" />
-                <NavLi title="Жюри" />
-                <NavLi title="Галерея" />
-                <NavLi title="Местоположение" />
-            </ul>
-        </nav>
-    </div>
-);
+export default class Header extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            background: false
+        }
+    }
+
+    onScroll = () => {
+        const background = this.state.background;
+
+        if (background && window.scrollY < 10) {
+            this.setState({background: false});
+        }
+
+        if (!background && window.scrollY > 10) {
+            this.setState({background: true});
+        }
+
+    }
+
+    componentWillMount() {
+        window.addEventListener('scroll', this.onScroll);
+    }
+
+    componentWIllUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+    }
+
+    render() {
+
+        const { title, showMainLink } = this.props;
+        return (<div {...ClassName({[styles.background]: this.state.background}, styles.header)}>
+            <div className={styles.title}>{title}</div> 
+            <nav className={styles.navigation}>
+                <ul>
+                    {showMainLink && <NavLi href='/' title="Главная" />}
+                    <NavLi title="О нас" />
+                    <NavLi title="Жюри" />
+                    <NavLi title="Галерея" />
+                    <NavLi title="Местоположение" />
+                </ul>
+            </nav>
+        </div>);
+    }
+}
