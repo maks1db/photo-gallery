cf299c6af4da7c7f1b71c87c13d1bf21f6114e94
maxim.skvortsov <maksskvor@yandex.ru>
2018-10-27 20:10:47 +0300

    prepare for 2018 event

--- src/backend/server/index.js
+++ src/backend/server/index.js
@@ -1,22 +1,40 @@
-const express = require('express');                      
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('../routes');
 const db = require('../db');
 
-const app = express();  
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //app.use('/',express.static(path.join(__dirname, '../../../public/favicon.ico')));
 
+app.use(
+    '/uploads',
+    express.static(path.join(__dirname, '../../../public/uploads'))
+);
+app.use(
+    '/uploads',
+    express.static(path.join(__dirname, '../../../public/uploads'))
+);
+
 if (process.env.NODE_ENV === 'dev') {
-    app.use('/assets',express.static(path.join(__dirname, '../../../public/assets')));
+    app.use(
+        '/assets',
+        express.static(path.join(__dirname, '../../../public/assets'))
+    );
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH'
+        );
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+
         if (req.method === 'OPTIONS') {
             return res.send(200);
         } else {
@@ -28,11 +46,11 @@ if (process.env.NODE_ENV === 'dev') {
 const port = 4000;
 
 app.use('', routes);
-app.get('*',function(req,res){
+app.get('*', function(req, res) {
     res.sendFile(path.resolve(__dirname, '../../../public/', 'index.html'));
 });
 
 db.connect();
-app.listen(port, ()=> console.log('Server PHOTO-GALLERY RUNNING on ' + port));
+app.listen(port, () => console.log('Server PHOTO-GALLERY RUNNING on ' + port));
 
 module.exports = app;
